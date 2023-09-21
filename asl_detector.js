ba405ee86eac7b69da8391cde20c7fd4d2e5022b
andres2212 <80753262+andres2212@users.noreply.github.com>
2023-09-21 06:19:20 -0600

    Update asl_detector.js

--- asl_detector.js
+++ asl_detector.js
@@ -1,178 +1,242 @@
-const video = document.getElementById('video');
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-let session;
-let metadata;
+let animationFrameId = null;
+let isProcessing = false;
+        if (animationFrameId) cancelAnimationFrame(animationFrameId);
 
-async function fetchMetadata() {
-    const response = await fetch('metadata2.yaml');
-    const text = await response.text();
-    const data = jsyaml.load(text);
-    metadata = data.names;
+        const resultCanvas = document.getElementById('resultCanvas');
+        const ctx = resultCanvas.getContext('2d');
+        ctx.clearRect(0, 0, resultCanvas.width, resultCanvas.height);
+
+let stream;
+let session
+
+let displayWidth;
+let displayHeight;
+const classes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+const colors = Array.from({ length: classes.length }, () => [Math.random() * 255, Math.random() * 255, Math.random() * 255]);
+
+async function processVideoFrame() {
+    const video = document.getElementById('video');
+    if (video.paused || video.ended) return;
+    const canvas = document.getElementById('resultCanvas');
+    const ctx = canvas.getContext('2d');
+
+    if (video.videoWidth === 0 || video.videoHeight === 0) return;
+    ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+
+    const inputTensor = await preprocessVideoFrame(video);
+    const modelUrl = 'best416.onnx';
+    await main(modelUrl, inputTensor);
+
+    if (isProcessing) requestAnimationFrame(processVideoFrame);
 }
 
-async function init() {
-    await fetchMetadata();
-    await setupModel();
-    navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-        video.srcObject = stream;
-        video.addEventListener('loadeddata', () => {
-            detectObjects();
-        });
-    });
+async function preprocessVideoFrame(video) {
+    let canvas = document.createElement('canvas');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    let ctx = canvas.getContext('2d');
+    if (video.videoWidth === 0 || video.videoHeight === 0) return;
+    ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let matObj = cv.matFromImageData(imageData);
+
+    let mat = new cv.Mat(matObj.rows, matObj.cols, cv.CV_8UC3);
+    cv.cvtColor(matObj, mat, cv.COLOR_RGBA2RGB);
+    let blackCanvas = new cv.Mat.zeros(Math.max(mat.cols, mat.rows), Math.max(mat.cols, mat.rows), cv.CV_8UC3);
+    let roi = blackCanvas.roi(new cv.Rect(0, 0, mat.cols, mat.rows));
+    mat.copyTo(roi);
+    const blob = cv.blobFromImage(blackCanvas, 1.0 / 255.0, new cv.Size(416, 416), new cv.Scalar(0, 0, 0), false, false, cv.CV_32F);
+    const inputT = new ort.Tensor('float32', blob.data32F, [1, 3, 416, 416]);
+
+    return inputT;
 }
 
-async function setupModel() {
-    const options = { executionProviders: ['cpu'] };
-    session = await ort.InferenceSession.create('best12.onnx', options);
+async function runObjectDetection(session, inputImage) {
+    const [height, width] = [inputImage.height, inputImage.width];
+    console.log("Height: ", height)
+    console.log("imaage: ", inputImage)
+
+    const length = Math.max(height, width);
+    const scale = length / 416;
+
+    const inputTensor = await preprocessVideoFrame(inputImage);
+    const { output0 } = await session.run({ images: inputTensor });
+
+    return { output0, scale };
 }
-
-//i changed from 640 to 64 and also changed the model just in case
-async function detectObjects() {
-    ctx.drawImage(video, 0, 0, 640, 640);
-    const imageData = ctx.getImageData(0, 0, 640, 640);
-
-    // Convert the input data to Float32Array and normalize it to the range [0, 1]
-    const numPixels = imageData.data.length / 4;
-    const imageDataArray = new Float32Array(numPixels * 3);
-    for (let i = 0; i < numPixels; i++) {
-        imageDataArray[i] = imageData.data[i * 4] / 255.0;
-        imageDataArray[numPixels + i] = imageData.data[i * 4 + 1] / 255.0;
-        imageDataArray[numPixels * 2 + i] = imageData.data[i * 4 + 2] / 255.0;
-    }
-
-    // Create the input tensor using the converted and normalized data
-    const inputTensor = new ort.Tensor('float32', imageDataArray, [1, 3, 640, 640]);
-    //let inputTensor
-    //inputTensor = new ort.Tensor('float32', new Float32Array(1 * 3 * 64 * 64), [1, 3, 64, 64]);
-
-    // Run the model with the input tensor
-    const inputName = session.inputNames[0];
-    const outputMap = await session.run({ [inputName]: inputTensor });
-    console.log("Outputmap: ",outputMap)
-    console.log("Outputmap length: ",outputMap.length)
-    
-    const outputNames = session.outputNames;
-    //console.log(outputNames);
-    const outputData = outputMap[outputNames[0]].data;
-    console.log(outputData)
-    console.log(outputData.length)
-    
-
-    //const outputData = outputMap.values().next().value.data;
-
-    processBoundingBoxes(outputData);
-
-    requestAnimationFrame(detectObjects);
-}
-
-
-
-
 
 function drawBoundingBox(ctx, classId, confidence, x, y, xPlusW, yPlusH) {
-    const label = `${metadata[classId]} (${confidence.toFixed(2)})`;
-    const color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+    const label = `${classes[classId]} (${confidence.toFixed(2)})`;
+    console.log(classId)
+    console.log(confidence)
+
+    const color = `rgb(${colors[classId].join(',')})`;
     ctx.strokeStyle = color;
     ctx.lineWidth = 2;
     ctx.strokeRect(x, y, xPlusW - x, yPlusH - y);
-    ctx.font = '14px Arial';
+    ctx.font = '16px sans-serif';
     ctx.fillStyle = color;
     ctx.fillText(label, x - 10, y - 10);
-    //console.log(label)
-    //console.log(classId)
-    
 }
+function transpose(tensor) {
+    const [batch, rows, cols] = tensor.dims;
+    const transposed = new Float32Array(cols * rows);
+    const data = tensor.data;
 
-function processBoundingBoxes(outputData) {
-    const boxes = [];
-    const scores = [];
-    const classIds = [];
-
-
-
-    for (let i = 0; i < outputData.length / 7; i++) {
-        const row = outputData.slice(i * 7, i * 7 + 7);
-        const [x, y, w, h, _, ...classScores] = row;
-        const maxScore = Math.max(...classScores);
-        const maxClassIndex = classScores.indexOf(maxScore);
-
-        if (maxScore >= 0.25) {
-            const box = [x - 0.5 * w, y - 0.5 * h, x + 0.5 * w, y + 0.5 * h];
-            boxes.push(box);
-            scores.push(maxScore);
-            classIds.push(maxClassIndex);
+    for (let row = 0; row < rows; ++row) {
+        for (let col = 0; col < cols; ++col) {
+            transposed[col * rows + row] = data[row * cols + col];
         }
     }
-
-    const indices = nms(boxes, scores, 0.25, 0.45);
-
-    //console.log('classIds', classIds);
-    //console.log('classIds', classIds[4000]);
-    
-    //console.log('scores', scores);
-    //console.log('boxes', boxes);
-    //console.log('indices', indices);
-
-
-    if (scores.length > 0) {
-        const maxConfidenceIndex = scores.indexOf(Math.max(...scores));
-        if (indices.includes(maxConfidenceIndex)) {
-            const [x, y, xPlusW, yPlusH] = boxes[maxConfidenceIndex].map(v => v * 640);
-            drawBoundingBox(ctx, classIds[maxConfidenceIndex], scores[maxConfidenceIndex], x, y, xPlusW, yPlusH);
-        }
-    }
+    return new ort.Tensor(tensor.type, transposed, [cols, rows]);
 }
 
-function nms(boxes, scores, scoreThreshold, nmsThreshold) {
-    const indices = scores.reduce((indices, score, i) => {
-        if (score >= scoreThreshold) {
-            indices.push(i);
-        }
-        return indices;
-    }, []);
+async function main(modelUrl) {
+    session = await ort.InferenceSession.create(modelUrl);
+    const webcamVideo = document.getElementById('video');
+    const canvas = document.getElementById('resultCanvas');
+    const ctx = canvas.getContext('2d');
 
-    indices.sort((a, b) => scores[b] - scores[a]);
+    async function detectFrame() {
+        //!!!!
+        const xScale = 1;
+        const yScale = 1;
 
-    const picked = [];
+        const { output0, scale } = await runObjectDetection(session, webcamVideo);
+        console.log(output0)
+        console.log("Scale: ", scale)
 
-    while (indices.length > 0) {
-        const i = indices.shift();
-        picked.push(i);
+        const outputData = transpose(output0);
 
-        const boxA = boxes[i];
+        const rows = outputData.dims[0];
 
-        indices.forEach((j, k) => {
-            const boxB = boxes[j];
-            const iou = computeIOU(boxA, boxB);
+        //test txt file, blob color rgb
+        const boxes = [];
+        const scores = [];
+        const classIds = [];
 
-            if (iou >= nmsThreshold) {
-                indices.splice(k, 1);
+        for (let i = 0; i < rows; i++) {
+            const rowData = outputData.data.slice(i * 30, (i + 1) * 30);
+            const classScores = rowData.slice(4);
+
+            const maxScore = Math.max(...classScores);
+
+            const maxClassIndex = classScores.findIndex(score => score === maxScore);
+
+            if (maxScore >= 0.50) {
+                const box = [
+                    rowData[0] - (0.5 * rowData[2]), rowData[1] - (0.5 * rowData[3]),
+                    rowData[2], rowData[3]
+                ];
+                boxes.push(box);
+                scores.push(maxScore);
+                classIds.push(maxClassIndex);
             }
-        });
+
+            //console.log(scores)
+        }
+
+        const canvas = document.getElementById('resultCanvas');
+        canvas.width = displayWidth;
+        canvas.height = displayHeight;
+
+        ctx.drawImage(webcamVideo, 0, 0, displayWidth, displayHeight);
+
+        if (scores.length > 0) {
+            const maxScoreIndex = scores.indexOf(Math.max(...scores));
+
+            console.log("Class ID: ", classIds[maxScoreIndex])
+
+            const box = boxes[maxScoreIndex];
+            drawBoundingBox(ctx, classIds[maxScoreIndex], scores[maxScoreIndex],
+                Math.round(box[0] * scale * xScale),
+                Math.round(box[1] * scale * yScale),
+                Math.round((box[0] + box[2]) * scale * xScale),
+                Math.round((box[1] + box[3]) * scale * yScale));
+        }
+        animationFrameId = requestAnimationFrame(detectFrame);
     }
 
-    return picked;
+    detectFrame();
 }
 
-function computeIOU(boxA, boxB) {
-    const [x1A, y1A, x2A, y2A] = boxA;
-    const [x1B, y1B, x2B, y2B] = boxB;
+async function startWebcam() {
 
-    const x1 = Math.max(x1A, x1B);
-    const y1 = Math.max(y1A, y1B);
-    const x2 = Math.min(x2A, x2B);
-    const y2 = Math.min(y2A, y2B);
+    var button = document.querySelector(".start");
+    
 
-    const intersectionArea = Math.max(0, x2 - x1) * Math.max(0, y2 - y1);
-    const boxAArea = (x2A - x1A) * (y2A - y1A);
-    const boxBArea = (x2B - x1B) * (y2B - y1B);
 
-    const unionArea = boxAArea + boxBArea - intersectionArea;
+    if (button.innerHTML === "Start Webcam") {
+        button.innerHTML = "Stop cam";
+        button.classList.add("pressed");
 
-    return intersectionArea / unionArea;
+        const webcamVideo = document.getElementById('video');
+
+        if (!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
+            alert('Your browser does not support webcam access.');
+            return;
+        }
+
+        stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        webcamVideo.srcObject = stream;
+
+        await new Promise((resolve) => {
+            webcamVideo.addEventListener('loadedmetadata', resolve);
+        });
+        webcamVideo.play();
+
+        const targetWidth = 426;
+        const targetHeight = 240;
+
+        const widthFactor = targetWidth / webcamVideo.videoWidth;
+        const heightFactor = targetHeight / webcamVideo.videoHeight;
+
+        const scale = Math.min(widthFactor, heightFactor);
+
+        displayWidth = webcamVideo.videoWidth * scale;
+        displayHeight = webcamVideo.videoHeight * scale;
+
+        video.width = displayWidth;
+        video.height = displayHeight;
+
+        console.log(displayWidth)
+        console.log(displayHeight)
+
+        const canvas = document.getElementById('resultCanvas');
+        canvas.width = displayWidth;
+        canvas.height = displayHeight;
+
+        const modelUrl = 'best416.onnx';
+        //best12 = 640x640
+        //best = 64x64
+        isProcessing = true;
+        await main(modelUrl);
+
+    } else {
+        button.innerHTML = "Start Webcam";
+        button.classList.remove("pressed");
+
+        
+            let tracks = stream.getTracks();
+            tracks.forEach(function(track) {
+                track.stop();
+            });
+            let video = document.getElementById('video');
+            video.srcObject = null;
+            session = null;
+        isProcessing = false;
+        if (animationFrameId) cancelAnimationFrame(animationFrameId);
+
+        const resultCanvas = document.getElementById('resultCanvas');
+        const ctx = resultCanvas.getContext('2d');
+        ctx.clearRect(0, 0, resultCanvas.width, resultCanvas.height);
+
+        
+
+    }
+
+
+
 }
-
-
-init();
-
